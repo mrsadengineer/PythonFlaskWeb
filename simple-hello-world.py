e5a7b778df6bee7b409907e93433ba06c114be1a
Sam <dsaeresearch@gmail.com>
2020-11-30 21:31:15 -0500

    updated simple hello world - vscode got to definit

--- simple-hello-world.py
+++ simple-hello-world.py
@@ -1,1 +1,29 @@
-print("Hello world")
+print("Hello Simple World")
+print("Time Date")
+
+from flask import Flask
+from datetime import datetime
+#import re
+
+app = Flask(__name__)
+
+
+@app.route("/itemoftime")
+def homeoftimehome():
+    return "Hello, Time!"
+
+
+@app.route("/itemoftime/<name>")
+def UnitOfTime(name):
+    #string something = 
+    #timetoremember = datetime.combine()
+
+    
+    print(name)
+    content = name[3]
+    return content
+
+
+
+print("http://127.0.0.1:5000/itemoftime")
+app.run()
